3e112279ed51491cf13c08775b9a7eb4b8b3d9cb
M1ha4iK <52006311+M1ha4iK@users.noreply.github.com>
2021-09-22 19:14:24 +0300

    fix

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default function dateSample(sampleRate) {
+  const rate = parseFloat(sampleRate)
+  if (typeof sampleRate !== 'string' || !isFinite(rate) || rate <= 0 || rate >= 15) return false 
+
+  const k = 0.693 / HALF_LIFE_PERIOD
+
+  return Math.ceil( Math.log(MODERN_ACTIVITY/rate) / k )
 }
